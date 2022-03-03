c753eb88555190810313e9664081fa1f1b60bb03
Piotr Smialkowski <smiaucius@gmail.com>
2022-03-03 10:34:29 -0600

    us-01 complete

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import ReservationForm from "./ReservationForm";
+import ErrorAlert from "../layout/ErrorAlert";
+import { createReservation } from "../utils/api";
 
 function NewReservation() {
   const history = useHistory();
@@ -15,22 +17,48 @@ function NewReservation() {
   };
 
   const [formData, setFormData] = useState({ ...initialFormState });
+  const [reservationsError, setReservationsError] = useState(null);
 
   const handleChange = ({ target }) => {
+    let value = target.value;
+    if (target.name === "people") {
+      value = Number(value);
+    }
     setFormData({
       ...formData,
-      [target.name]: target.value,
+      [target.name]: value,
     });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const abortController = new AbortController();
+    async function addReservation() {
+      try {
+        await createReservation({ data: formData }, abortController.signal);
+        history.push(`/dashboard?date=${formData.reservation_date}`);
+      } catch (error) {
+        setReservationsError(error);
+      }
+    }
+    addReservation();
+    return () => abortController.abort();
   };
 
   return (
     <>
       <h1>Create Reservation</h1>
+      <ErrorAlert error={reservationsError} />
       <ReservationForm formData={formData} handleChange={handleChange} />
       <button className="btn btn-secondary mr-2" onClick={history.goBack}>
         Cancel
       </button>
-      <button form="reservationForm" type="submit" className="btn btn-primary">
+      <button
+        form="reservationForm"
+        type="submit"
+        className="btn btn-primary"
+        onClick={handleSubmit}
+      >
         Submit
       </button>
     </>
